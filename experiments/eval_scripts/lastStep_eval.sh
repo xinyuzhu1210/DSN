9044a8d0083b0ddff5eb671bfd829eab9b646a0f
Xinyu Zhu <xyz.hm@hotmail.com>
2026-02-03 20:24:24 +0100

    qwen eval 2

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -16,7 +16,7 @@ export model=llama31 # choose from: gemma, gemma2, llama2, llama2_13b, llama3, l
 # ../results_dsn/20260203-02:07:25 1E0/qwen_dsn_25_offset0.json
 # ../results_dsn/20260203-02:09:25 1E0/qwen_dsn_25_offset0.json
 file_list=(
-    '../results_dsn/20260203-02:01:22 1E0/qwen_dsn_25_offset0.json'
+    '../results_dsn/20260203-02:04:40 1E0/qwen_dsn_25_offset0.json'
 )
 
 for file_path in "${file_list[@]}"
