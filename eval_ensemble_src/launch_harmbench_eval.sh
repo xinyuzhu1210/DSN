a8fe7e24663fb36ec9af4934ddc3ecf8adfd68e4
Xinyu Zhu <xyz.hm@hotmail.com>
2026-02-03 02:47:15 +0100

    test harmbench

---
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sample file path
+# ../experiments/results_dsn/20260127-03:30:44 1E0/llama31_dsn_25_offset0.json
+# ../experiments/results_dsn/20260127-14:25:13 1E0/llama31_dsn_25_offset0.json
+# ../experiments/results_dsn/20260128-01:18:36 1E0/llama31_dsn_25_offset0.json
+# ../experiments/results_dsn/20260128-13:47:22 1E0/llama31_dsn_25_offset0.json
+STRING_ARRAY=(
+    './experiments/results_dsn/20260127-03:30:44 1E0/llama31_dsn_25_offset0.json'
+)
+
+for STR in "${STRING_ARRAY[@]}"; do
+  python harmbench_only_evaluation.py -1 "$STR" False
+done                              # random_seed, logfile, UseJBB
