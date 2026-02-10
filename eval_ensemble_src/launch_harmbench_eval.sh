b5889a2d5fd6e22e3ed4a62a9a96506f34071522
Xinyu Zhu <xyz.hm@hotmail.com>
2026-02-10 14:37:45 +0100

    eval harmbench JBB

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -41,7 +41,7 @@
 # ../experiments/evalLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalLastStep/20260209-19:58:46 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json
-# qwen batch_size 41, 42, 43, 44, 45, aug_sampling = false, --> JBB
+# qwen batch_size 41, 42, 43, 44, 45, aug_sampling = false, --> JBB True
 # ../experiments/evalJBBLastStep/20260206-17:12:43 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260209-19:52:54 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json
@@ -49,13 +49,13 @@
 # ../experiments/evalJBBLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalLastStep/20260206-17:12:43 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalLastStep/20260209-19:52:54 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalLastStep/20260209-19:58:46 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260206-17:12:43 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260209-19:52:54 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260209-19:58:46 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py -1 "$STR" False
+  python harmbench_only_evaluation.py -1 "$STR" True
 done                              # random_seed, logfile, UseJBB
