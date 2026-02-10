6a9ba913ec2aa612572685ad9878be400318a36e
Xinyu Zhu <xyz.hm@hotmail.com>
2026-02-11 00:27:19 +0100

    harmbench aug sampling true, advbench

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -47,13 +47,25 @@
 # ../experiments/evalJBBLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260209-19:58:46 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json
+# qwen batch_size 41, 42, 43, 44, 45, aug_sampling = true
+# ../experiments/evalLastStep/20260209-23:12:54 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260210-15:16:19 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260210-14:50:10 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260210-15:06:41 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260210-15:08:15 1E0/qwen_dsn_25_offset0.json
+# qwen batch_size 41, 42, 43, 44, 45, aug_sampling = true, --> JBB True
+# ../experiments/evalJBBLastStep/20260209-23:12:54 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260210-15:16:19 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260210-14:50:10 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260210-15:06:41 1E0/qwen_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260210-15:08:15 1E0/qwen_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalJBBLastStep/20260206-17:12:43 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalJBBLastStep/20260209-19:52:54 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalJBBLastStep/20260209-19:56:48 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalJBBLastStep/20260209-19:58:46 1E0/qwen_dsn_25_offset0.json'
-  '../experiments/evalJBBLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260209-23:12:54 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260210-15:16:19 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260210-14:50:10 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260210-15:06:41 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260210-15:08:15 1E0/qwen_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
