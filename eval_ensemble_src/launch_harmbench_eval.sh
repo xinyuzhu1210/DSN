7cba502f71d446ee137dbc51f2cdec1dce9c4764
Xinyu Zhu <xyz.hm@hotmail.com>
2026-03-24 23:35:45 +0100

    harmbench advbench seed 41

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -73,6 +73,7 @@
 # ../experiments/evalJBBLastStep/20260214-00:41:13 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260214-00:42:30 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260214-00:43:43 1E0/qwen_dsn_25_offset0.json
+
 # Qwen2-7B train, batch size 256, seed 41, 42, 43, 44, 45, use_aug_sampling=False, doubled prefixes list
 # ../experiments/evalLastStep/20260214-01:14:28 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalLastStep/20260214-01:17:29 1E0/qwen_dsn_25_offset0.json
@@ -85,6 +86,7 @@
 # ../experiments/evalJBBLastStep/20260214-01:18:57 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260214-01:20:00 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260216-18:34:40 1E0/qwen_dsn_25_offset0.json
+
 # Qwen2-7B train, batch size 256, seed 41, 42, 43, 44, 45, use_aug_sampling=False, Magic integration
 # ../experiments/evalLastStep/20260311-04:27:52 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalLastStep/20260311-04:30:23 1E0/qwen_dsn_25_offset0.json
@@ -105,9 +107,9 @@
 # ../experiments/evalJBBLastStep/20260206-17:14:48 1E0/qwen_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalJBBLastStep/20260209-20:22:30 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260214-01:14:28 1E0/qwen_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py 45 "$STR" True
+  python harmbench_only_evaluation.py 41 "$STR" False
 done                              # random_seed, logfile, UseJBB
