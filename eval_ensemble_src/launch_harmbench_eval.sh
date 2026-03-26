8fe8e724a1f4a4e196a2a65046c81472c286d843
Xinyu Zhu <xyz.hm@hotmail.com>
2026-03-26 19:50:29 +0100

    error; reevaluate harmbench advbench seed 41

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -119,9 +119,9 @@
 # ../experiments/evalJBBLastStep/20260326-02:24:37 1E0/qwen_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalJBBLastStep/20260326-02:24:37 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260326-02:09:41 1E0/qwen_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py 45 "$STR" True
+  python harmbench_only_evaluation.py 41 "$STR" False
 done                              # random_seed, logfile, UseJBB
