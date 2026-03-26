6875cb31a075c777570cf0104c5ba7527df8217b
Xinyu Zhu <xyz.hm@hotmail.com>
2026-03-27 00:19:42 +0100

    test qwen

--- eval_ensemble_src/eval4_HarmBench.py
+++ eval_ensemble_src/eval4_HarmBench.py
@@ -145,7 +145,7 @@ def get_HarmBench_results(goal, answer, ctrl, contains_suffix = True):
                 max_new_tokens=5,
             )
             # remove the prompt ids from the output ids, as both the prompt and answer are returned
-            output_ids = output_ids[:, len(encoded.input_ids.shape[1]):]
+            output_ids = output_ids[:, encoded.input_ids.shape[1]:]
         # decode output into text
         completion = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip().lower()
         # interpret results
