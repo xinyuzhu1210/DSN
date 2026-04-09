ab51d926c970e46b6a126be0b3394abd66a8fc2e
Xinyu Zhu <xyz.hm@hotmail.com>
2026-04-09 23:48:23 +0200

    test

--- llm_attacks/base/attack_manager.py
+++ llm_attacks/base/attack_manager.py
@@ -905,6 +905,7 @@ class AttackPrompt(object):
         topk_values, _ = torch.topk(stacked_tensor,k=5,dim=0)
         loss = torch.mean(topk_values, dim=0)
         print("stacked tensor shape", stacked_tensor.shape)
+        print("stacked tensor", stacked_tensor)
         print("values", topk_values.shape)
         print("loss", loss)
         print("loss shape", loss.shape)
