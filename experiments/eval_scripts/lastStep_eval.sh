1502b80613f7ae4f3af931a39682b0baef5612ea
Xinyu Zhu <xyz.hm@hotmail.com>
2026-04-15 22:02:50 +0200

    max() operation triples list DSN eval Refusal Matching

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -63,9 +63,11 @@ export model=qwen # choose from: gemma, gemma2, llama2, llama2_13b, llama3, llam
 # ../results_dsn/20260326-02:24:37 1E0/qwen_dsn_25_offset0.json
 # Qwen tripled list seed 42 top-k average
 # ../results_dsn/20260410-01:04:26 1E0/qwen_dsn_25_offset0.json
+# Qwen tripled list seed 42 max() operation
+# ../results_dsn/20260414-00:58:27 1E0/qwen_dsn_25_offset0.json
 
 file_list=(
-    '../results_dsn/20260410-01:04:26 1E0/qwen_dsn_25_offset0.json'
+    '../results_dsn/20260414-00:58:27 1E0/qwen_dsn_25_offset0.json'
 )
 
 for file_path in "${file_list[@]}"
