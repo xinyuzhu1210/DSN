25858678489f7d1304fcfca15b3b7215ce07f35f
Xinyu Zhu <xyz.hm@hotmail.com>
2026-04-22 19:54:59 +0200

    original list max()

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -79,7 +79,7 @@ export model=qwen # choose from: gemma, gemma2, llama2, llama2_13b, llama3, llam
 
 
 file_list=(
-    '../results_dsn/20260414-02:57:13 1E0/qwen_dsn_25_offset0.json'
+    '../results_dsn/20260414-02:54:44 1E0/qwen_dsn_25_offset0.json'
 )
 
 for file_path in "${file_list[@]}"
