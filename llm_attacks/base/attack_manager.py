8bc98d62347d18cbc414a4ad1c9126305b9625ef
Xinyu Zhu <xyz.hm@hotmail.com>
2026-05-05 01:42:43 +0200

    test var print statements

--- llm_attacks/base/attack_manager.py
+++ llm_attacks/base/attack_manager.py
@@ -901,7 +901,9 @@ class AttackPrompt(object):
                 loss += temp_loss.mean(-1)
                 count_loss += 1
 
-                all_losses.append(temp_loss.mean(-1))
+                # all_losses.append(temp_loss.mean(-1))
+                if torch.var(temp_loss.mean(-1)) > 1e-6:
+                    all_losses.append(temp_loss.mean(-1))
 
         # average the loss over all sliding windows
         loss = loss/count_loss
