eb9c2fbbfc98c73ca6e2b64e5764d0a052b39499
Xinyu Zhu <xyz.hm@hotmail.com>
2026-05-05 02:12:38 +0200

    .

--- llm_attacks/base/attack_manager.py
+++ llm_attacks/base/attack_manager.py
@@ -902,7 +902,7 @@ class AttackPrompt(object):
                 count_loss += 1
 
                 # all_losses.append(temp_loss.mean(-1))
-                if torch.var(temp_loss.mean(-1)) > 1e-8:
+                if torch.var(temp_loss.mean(-1)) > 1e-5:
                     all_losses.append(temp_loss.mean(-1))
                     loss += temp_loss.mean(-1)
                     count_loss += 1
