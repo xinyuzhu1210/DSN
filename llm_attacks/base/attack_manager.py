a13a400c19799e3b37db4ec35ca5e9173937db7e
Xinyu Zhu <xyz.hm@hotmail.com>
2026-05-27 04:33:19 +0200

    test beam candidates print statement

--- llm_attacks/base/attack_manager.py
+++ llm_attacks/base/attack_manager.py
@@ -2067,6 +2067,7 @@ class EvaluateAttack(object):
                     targets = [p.target for p in attack.prompts[0]._prompts]
                     all_outputs = []
                     gen_inputs = []
+                    candidate_outputs = []
                     if len(all_inputs) // batch_size == len(all_inputs) / batch_size:
                         temp_length = len(all_inputs) // batch_size
                     else:
@@ -2114,20 +2115,13 @@ class EvaluateAttack(object):
                         
                         # beam candidates version
                         batch_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-                        print(batch_outputs)
                         gen_start_idx = [len(tokenizer.decode(batch_input_ids[i], skip_special_tokens=True)) for i in range(len(batch_input_ids))]
                         gen_start_idx_nr_beams_duplicate = [x for x in gen_start_idx for _ in range(9)]
-                        print(gen_start_idx_nr_beams_duplicate)
                         # remove prompt part from the output string
                         batch_outputs = [output[gen_start_idx_nr_beams_duplicate[i]:] for i, output in enumerate(batch_outputs)]
-                        print(batch_outputs)
-                        print(len(batch_outputs))
                         slice_nr = 9 
                         sliced_beam_outputs = [batch_outputs[i:i + slice_nr] for i in range(0, len(batch_outputs), slice_nr)]
-                        print(sliced_beam_outputs)
-                        print(len(sliced_beam_outputs))
                         batch_outputs = [output_list[0] for output_list in sliced_beam_outputs]
-                        print(batch_outputs)
 
                         # # decode output token ids into text/string
                         # batch_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
@@ -2140,6 +2134,7 @@ class EvaluateAttack(object):
                         prompt_inputs = batch
                         all_outputs.extend(batch_outputs)
                         gen_inputs.extend(prompt_inputs)
+                        candidate_outputs.extend(sliced_beam_outputs)
                         # print(all_outputs)
                         print(len(all_outputs))
                         # print(gen_inputs)
@@ -2152,12 +2147,15 @@ class EvaluateAttack(object):
 
                     curr_jb, curr_em = [], []
                     # after all batches
-                    for (gen_str, target, input_str) in zip(all_outputs, targets, gen_inputs):
+                    # for (gen_str, target, input_str) in zip(all_outputs, targets, gen_inputs):
+                    # beam candidates version
+                    for (gen_str, target, input_str, beam_candidates) in zip(all_outputs, targets, gen_inputs, candidate_outputs):
                     # for (gen_str, target) in zip(all_outputs, targets):
                         # jailbroken if the generated response does not contain any refusal prefixes
                         print("input string:", input_str)
                         print("target string:", target)
                         print("generated string:", gen_str)
+                        print("beam candidates", beam_candidates)
                         # print("classifier 1", pipe(gen_str))
                         print("classifier 2", pipe_1(gen_str))
                         output_class = pipe_1(gen_str)
