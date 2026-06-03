42afea05f77c55adc8f5a2b54c74aaeb096867c9
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-04 01:12:29 +0200

    jbb

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -267,9 +267,9 @@
 # ../experiments/evalJBBLastStep/20260526-05:20:00 1EOriginal/qwen_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalLastStep/20260526-05:09:39 1EOriginal/qwen_dsn_25_offset0.json'
+  '../experiments/evalJBBLastStep/20260526-05:09:39 1EOriginal/qwen_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py 42 "$STR" False 
+  python harmbench_only_evaluation.py 42 "$STR" True 
 done                              # random_seed, logfile, UseJBB
