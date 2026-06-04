df2ec75a1f6d315a54ee34ffc73af632ececf7d7
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-05 01:33:25 +0200

    llama3.1 no seed advbench suffix 1

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -279,10 +279,24 @@
 # ../experiments/evalJBBLastStep/20260203-02:07:25 1E0/qwen_dsn_25_offset0.json
 # ../experiments/evalJBBLastStep/20260203-02:09:25 1E0/qwen_dsn_25_offset0.json
 
+# llama31 no seed 
+# ../experiments/evalLastStep/20260127-03:30:44 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260127-14:25:13 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260128-01:18:36 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260128-13:47:22 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalLastStep/20260603-05:45:04 1E0/llama31_dsn_25_offset0.json
+# -JBB llama31 --> JBB True
+# ../experiments/evalJBBLastStep/20260127-03:30:44 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260127-14:25:13 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260128-01:18:36 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260128-13:47:22 1E0/llama31_dsn_25_offset0.json
+# ../experiments/evalJBBLastStep/20260603-05:45:04 1E0/llama31_dsn_25_offset0.json
+
+
 STRING_ARRAY=(
-  '../experiments/evalJBBLastStep/20260203-02:09:25 1E0/qwen_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260127-03:30:44 1E0/llama31_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py -1 "$STR" True 
+  python harmbench_only_evaluation.py -1 "$STR" False 
 done                              # random_seed, logfile, UseJBB
