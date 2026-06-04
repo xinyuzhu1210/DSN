d177324c3d86996f02c143581b2291ffef36ebdc
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-05 00:48:59 +0200

    4th suffix

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -123,7 +123,7 @@ export model=qwen # choose from: gemma, gemma2, llama2, llama2_13b, llama3, llam
 
 
 file_list=(
-    '../results_dsn/20260203-02:06:12 1E0/qwen_dsn_25_offset0.json'
+    '../results_dsn/20260203-02:07:25 1E0/qwen_dsn_25_offset0.json'
 )
 
 for file_path in "${file_list[@]}"
