fedca8d133d30631d9af083a4e4ccc2929a6885c
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-06 03:25:02 +0200

    44

--- eval_ensemble_src/launch_harmbench_eval.sh
+++ eval_ensemble_src/launch_harmbench_eval.sh
@@ -306,9 +306,9 @@
 # ../experiments/evalJBBLastStep/20260603-05:58:48 1E0/llama31_dsn_25_offset0.json
 
 STRING_ARRAY=(
-  '../experiments/evalJBBLastStep/20260603-05:56:13 1E0/llama31_dsn_25_offset0.json'
+  '../experiments/evalLastStep/20260603-05:57:22 1E0/llama31_dsn_25_offset0.json'
 )
 
 for STR in "${STRING_ARRAY[@]}"; do
-  python harmbench_only_evaluation.py 43 "$STR" True 
+  python harmbench_only_evaluation.py 44 "$STR" False 
 done                              # random_seed, logfile, UseJBB
