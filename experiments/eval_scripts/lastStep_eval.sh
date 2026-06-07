ad4938fc92b4c4aec2534e2448d4d72d68e04988
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-07 06:21:46 +0200

    44

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -172,7 +172,7 @@ export model=llama31 # choose from: gemma, gemma2, llama2, llama2_13b, llama3, l
 
 
 file_list=(
-    '../results_dsn/20260605-03:10:59 1E0/llama31_dsn_25_offset0.json'
+    '../results_dsn/20260605-03:12:22 1E0/llama31_dsn_25_offset0.json'
 )
 
 for file_path in "${file_list[@]}"
@@ -190,7 +190,7 @@ do
         --config.eval_max_new_len=512 \
         --config.eval_target_folder_name="" \
         --config.logfile="${file_path}" \
-        --config.random_seed_for_sampling_targets=43 \
+        --config.random_seed_for_sampling_targets=44 \
         --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
 
     # evaluate upon the JailbreakBench dataset
@@ -205,7 +205,7 @@ do
         --config.eval_max_new_len=512 \
         --config.eval_target_folder_name="evalJBBLastStep" \
         --config.logfile="${file_path}" \
-        --config.random_seed_for_sampling_targets=43 \
+        --config.random_seed_for_sampling_targets=44 \
         --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
 
 done
