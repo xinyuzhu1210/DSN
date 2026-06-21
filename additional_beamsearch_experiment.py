d66fcbb80f230e388572a09915d41423b45512e9
Xinyu Zhu <xyz.hm@hotmail.com>
2026-06-21 16:50:16 +0200

    additional files

---
+++ additional_beamsearch_experiment.py
@@ -0,0 +1,254 @@
+# advbench
+harmbench_prompts_beam1 = []
+harmbench_outputs_beam1 = []
+gemma4_prompts_beam1 = []
+gemma4_outputs_beam1 = []
+# get path to Harmbench and Gemma beam=1 or sampling output file for the advbench dataset 
+# with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam1/seed_45/harmbench_gemma_advbench/slurm_output_23031557.out', 'r') as file:
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/sampling/seed_45/harmbench_gemma_advbench/slurm_output_23030269.out', 'r') as file:
+    for line in file:
+        # at each goal/prompt, add the harmbench and gemma4 responses to the list
+        if "Harmbench goal" in line.strip():
+            harmbench_prompts_beam1.append(line.strip())
+        if "Harmbench classification" in line.strip():
+            harmbench_outputs_beam1.append(line.strip())
+        if "Gemma4 goal" in line.strip():
+            gemma4_prompts_beam1.append(line.strip())
+        if "Gemma4 classification" in line.strip():
+            gemma4_outputs_beam1.append(line.strip())
+
+# print(len(harmbench_prompts_beam1))
+# print(len(harmbench_outputs_beam1))
+# print(len(gemma4_prompts_beam1))
+# print(len(gemma4_outputs_beam1))
+# print(harmbench_outputs_beam1)
+
+harmbench_prompts_beam9 = []
+harmbench_outputs_beam9 = []
+gemma4_prompts_beam9 = []
+gemma4_outputs_beam9 = []
+
+# get path to Harmbench and Gemma beam=9 output file for the advbench dataset
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam9/seed_45/harmbench_gemma_advbench/slurm_output_23031018.out', 'r') as file:
+    for line in file:
+        # at each goal/prompt, add the harmbench and gemma4 responses to the list
+        if "Harmbench goal" in line.strip():
+            harmbench_prompts_beam9.append(line.strip())
+        if "Harmbench classification" in line.strip():
+            harmbench_outputs_beam9.append(line.strip())
+        if "Gemma4 goal" in line.strip():
+            gemma4_prompts_beam9.append(line.strip())
+        if "Gemma4 classification" in line.strip():
+            gemma4_outputs_beam9.append(line.strip())
+
+# print(len(harmbench_prompts_beam9))
+# print(len(harmbench_outputs_beam9))
+# print(len(gemma4_prompts_beam9))
+# print(len(gemma4_outputs_beam9))
+# print(gemma4_outputs_beam9)
+
+harmbench_correct_scores = 0
+# count total nr of rejections from the harmbench classifier in the beam=9 case
+harmbench_total_negatives_beam9 = harmbench_outputs_beam9.count("Harmbench classification No")
+gemma4_correct_scores = 0
+# count total nr of rejections from the Gemma4 classifier in the beam=9 case
+gemma4_total_negatives_beam9 = gemma4_outputs_beam9.count("Gemma4 classification no")
+
+harmbench_prompts = []
+gemma_prompts = []
+
+for i in range(len(harmbench_prompts_beam9)):
+    # count for harmbench and gemma4 how many from the rejected prompts in the beam=9 case, were actually accepted in the greedy (beam=1) case
+    if harmbench_outputs_beam9[i] == "Harmbench classification No" and harmbench_outputs_beam1[i] == "Harmbench classification Yes":
+        harmbench_correct_scores += 1
+        harmbench_prompts.append(f"prompt {i}: {harmbench_prompts_beam1[i]}")
+    if gemma4_outputs_beam9[i] == "Gemma4 classification no" and gemma4_outputs_beam1[i] == "Gemma4 classification yes":
+        gemma4_correct_scores += 1
+        gemma_prompts.append(f"prompt {i}: {gemma4_prompts_beam1[i]}")
+
+print("harmbench advbench prompts")
+for i in harmbench_prompts: 
+    print(i)
+
+print("gemma advbench prompts")
+for i in gemma_prompts: 
+    print(i)
+
+# print the total scores for both classifiers for the advbench dataset 
+print("Harmbench and Gemma4 advbench dataset")
+print(f"There are {harmbench_correct_scores}/{harmbench_total_negatives_beam9} ({(harmbench_correct_scores/harmbench_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for Harmbench.")
+print(f"There are {gemma4_correct_scores}/{gemma4_total_negatives_beam9} ({(gemma4_correct_scores/gemma4_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for Gemma4.")
+
+# print(harmbench_total_negatives_beam9)
+# print(gemma4_correct_scores)
+# print(gemma4_total_negatives_beam9)
+
+# jailbreakbench
+harmbench_prompts_beam1 = []
+harmbench_outputs_beam1 = []
+gemma4_prompts_beam1 = []
+gemma4_outputs_beam1 = []
+# get path to Harmbench and Gemma beam=1 or sampling output file for the jailbreakbench dataset
+# with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam1/seed_45/harmbench_gemma_jbb/slurm_output_23031694.out', 'r') as file:
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/sampling/seed_45/harmbench_gemma_jbb/slurm_output_23030378.out', 'r') as file:
+    for line in file:
+        # at each goal/prompt, add the harmbench and gemma4 responses to the list
+        if "Harmbench goal" in line.strip():
+            harmbench_prompts_beam1.append(line.strip())
+        if "Harmbench classification" in line.strip():
+            harmbench_outputs_beam1.append(line.strip())
+        if "Gemma4 goal" in line.strip():
+            gemma4_prompts_beam1.append(line.strip())
+        if "Gemma4 classification" in line.strip():
+            gemma4_outputs_beam1.append(line.strip())
+
+harmbench_prompts_beam9 = []
+harmbench_outputs_beam9 = []
+gemma4_prompts_beam9 = []
+gemma4_outputs_beam9 = []
+
+# get path to Harmbench and Gemma beam=9 output file for the jailbreakbench dataset
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam9/seed_45/harmbench_gemma_jbb/slurm_output_23031142.out', 'r') as file:
+    for line in file:
+        # at each goal/prompt, add the harmbench and gemma4 responses to the list
+        if "Harmbench goal" in line.strip():
+            harmbench_prompts_beam9.append(line.strip())
+        if "Harmbench classification" in line.strip():
+            harmbench_outputs_beam9.append(line.strip())
+        if "Gemma4 goal" in line.strip():
+            gemma4_prompts_beam9.append(line.strip())
+        if "Gemma4 classification" in line.strip():
+            gemma4_outputs_beam9.append(line.strip())
+
+harmbench_correct_scores = 0
+# count total nr of rejections from the harmbench classifier in the beam=9 case
+harmbench_total_negatives_beam9 = harmbench_outputs_beam9.count("Harmbench classification No")
+gemma4_correct_scores = 0
+# count total nr of rejections from the Gemma4 classifier in the beam=9 case
+gemma4_total_negatives_beam9 = gemma4_outputs_beam9.count("Gemma4 classification no")
+
+harmbench_prompts = []
+gemma_prompts = []
+
+for i in range(len(harmbench_prompts_beam9)):
+    # count for harmbench and gemma4 how many from the rejected prompts in the beam=9 case, were actually accepted in the greedy (beam=1) case
+    if harmbench_outputs_beam9[i] == "Harmbench classification No" and harmbench_outputs_beam1[i] == "Harmbench classification Yes":
+        harmbench_correct_scores += 1
+        harmbench_prompts.append(f"prompt {i}: {harmbench_prompts_beam1[i]}")
+    if gemma4_outputs_beam9[i] == "Gemma4 classification no" and gemma4_outputs_beam1[i] == "Gemma4 classification yes":
+        gemma4_correct_scores += 1
+        gemma_prompts.append(f"prompt {i}: {gemma4_prompts_beam1[i]}")
+
+print("harmbench jbb prompts")
+for i in harmbench_prompts: 
+    print(i)
+
+print("gemma jbb prompts")
+for i in gemma_prompts: 
+    print(i)
+
+# print the total scores for both classifiers for the jailbreakbench dataset
+print("Harmbench and Gemma4 jailbreakbench dataset")
+print(f"There are {harmbench_correct_scores}/{harmbench_total_negatives_beam9} ({(harmbench_correct_scores/harmbench_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for Harmbench.")
+print(f"There are {gemma4_correct_scores}/{gemma4_total_negatives_beam9} ({(gemma4_correct_scores/gemma4_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for Gemma4.")
+
+# Refusal Matching
+RM_targets_advbench_beam1 = []
+RM_outputs_advbench_beam1 = []
+RM_targets_jbb_beam1 = []
+RM_outputs_jbb_beam1 = []
+training_samples_passed = False
+jbb_dataset = False
+# get path to refusal matching beam=1 or sampling output file 
+# with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam1/seed_45/refusal_matching/slurm_output_23031310.out', 'r') as file:
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/sampling/seed_45/refusal_matching/slurm_output_23029910.out', 'r') as file:
+    for line in file:
+        # do not include the evaluation results from the advbench training data
+        if "Train Step" in line.strip():
+            training_samples_passed = True
+        # at each goal/prompt, add the refusal matching classifier responses of the advbench dataset to the list
+        if "target string" in line.strip() and training_samples_passed == True and jbb_dataset == False:
+            RM_targets_advbench_beam1.append(line.strip())
+        if "classifier 2" in line.strip() and training_samples_passed == True and jbb_dataset == False:
+            RM_outputs_advbench_beam1.append(line.strip())
+        # now only include samples of the jailbreakbench dataset
+        if "logfile= ../evalJBBLastStep" in line.strip() and training_samples_passed == True: 
+            jbb_dataset = True
+        # at each goal/prompt, add the refusal matching classifier responses of the jailbreakbench dataset to the list
+        if "target string" in line.strip() and training_samples_passed == True and jbb_dataset == True:
+            RM_targets_jbb_beam1.append(line.strip())
+        if "classifier 2" in line.strip() and training_samples_passed == True and jbb_dataset == True:
+            RM_outputs_jbb_beam1.append(line.strip())
+
+# print(len(RM_targets_advbench_beam1))
+# print(len(RM_targets_jbb_beam1))
+# print(len(RM_outputs_advbench_beam1))
+# print(len(RM_outputs_jbb_beam1))
+
+RM_targets_advbench_beam9 = []
+RM_outputs_advbench_beam9 = []
+RM_targets_jbb_beam9 = []
+RM_outputs_jbb_beam9 = []
+training_samples_passed = False
+jbb_dataset = False
+# get path to the refusal matching beam=9 output file 
+with open('../../../Desktop/beam_search_additional_experiment_qwen_DSN/beam9/seed_45/refusal_matching/slurm_output_23030825.out', 'r') as file:
+    for line in file:
+        # do not include the evaluation results from the advbench training data
+        if "Train Step" in line.strip():
+            training_samples_passed = True
+        # at each goal/prompt, add the refusal matching classifier responses of the advbench dataset to the list
+        if "target string" in line.strip() and training_samples_passed == True and jbb_dataset == False:
+            RM_targets_advbench_beam9.append(line.strip())
+        if "classifier 2" in line.strip() and training_samples_passed == True and jbb_dataset == False:
+            RM_outputs_advbench_beam9.append(line.strip())
+        # now only include samples of the jailbreakbench dataset
+        if "logfile= ../evalJBBLastStep" in line.strip() and training_samples_passed == True: 
+            jbb_dataset = True
+        # at each goal/prompt, add the refusal matching classifier responses of the jailbreakbench dataset to the list
+        if "target string" in line.strip() and training_samples_passed == True and jbb_dataset == True:
+            RM_targets_jbb_beam9.append(line.strip())
+        if "classifier 2" in line.strip() and training_samples_passed == True and jbb_dataset == True:
+            RM_outputs_jbb_beam9.append(line.strip())
+
+# print(len(RM_targets_advbench_beam9))
+# print(len(RM_targets_jbb_beam9))
+# print(len(RM_outputs_advbench_beam9))
+# print(len(RM_outputs_jbb_beam9))
+
+advbench_correct_scores = 0
+advbench_total_negatives_beam9 = 0
+jbb_correct_scores = 0
+jbb_total_negatives_beam9 = 0
+
+rm_advbench = []
+rm_jbb = []
+
+for i in range(len(RM_targets_advbench_beam9)):
+    # count how many from the rejected prompts in the beam=9 case, were actually accepted in the greedy (beam=1) case for the advbench dataset
+    if ("LABEL_1" in RM_outputs_advbench_beam9[i] or "LABEL_3" in RM_outputs_advbench_beam9[i]) and ("LABEL_0" in RM_outputs_advbench_beam1[i] or "LABEL_2" in RM_outputs_advbench_beam1[i] or "LABEL_4" in RM_outputs_advbench_beam1[i]):
+        advbench_correct_scores += 1
+        rm_advbench.append(f"prompt advbench {i}: {RM_targets_advbench_beam1[i]}")
+    # count total nr of rejections in the beam=9 case
+    if "LABEL_1" in RM_outputs_advbench_beam9[i] or "LABEL_3" in RM_outputs_advbench_beam9[i]:
+        advbench_total_negatives_beam9 += 1
+    # count how many from the rejected prompts in the beam=9 case, were actually accepted in the greedy (beam=1) case for the jailbreakbench dataset
+    if ("LABEL_1" in RM_outputs_jbb_beam9[i] or "LABEL_3" in RM_outputs_jbb_beam9[i]) and ("LABEL_0" in RM_outputs_jbb_beam1[i] or "LABEL_2" in RM_outputs_jbb_beam1[i] or "LABEL_4" in RM_outputs_jbb_beam1[i]):
+        jbb_correct_scores += 1
+        rm_jbb.append(f"prompt jbb {i}: {RM_targets_jbb_beam1[i]}")
+    # count total nr of rejections in the beam=9 case
+    if "LABEL_1" in RM_outputs_jbb_beam9[i] or "LABEL_3" in RM_outputs_jbb_beam9[i]:
+        jbb_total_negatives_beam9 += 1
+
+print("rm advbench prompts")
+for i in rm_advbench: 
+    print(i)
+
+print("rm jbb prompts")
+for i in rm_jbb: 
+    print(i)
+
+# print the total scores for refusal matching metric
+print("Refusal Matching")
+print(f"There are {advbench_correct_scores}/{advbench_total_negatives_beam9} ({(advbench_correct_scores/advbench_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for Advbench.")
+print(f"There are {jbb_correct_scores}/{jbb_total_negatives_beam9} ({(jbb_correct_scores/jbb_total_negatives_beam9) * 100}%) beam=1/sampling accepted cases for JBB")
---
+++ beam1_sampling_similarity_prompts.py
@@ -0,0 +1,180 @@
+harmbench_advbench = []
+gemma_advbench = []
+harmbench_jbb = []
+gemma_jbb = []
+rm_advbench_list = []
+rm_jbb_list = []
+
+h_advbench = False
+g_advbench = False
+h_jbb = False
+g_jbb = False
+rm_advbench = False
+rm_jbb = False
+
+# prompt file for beam=1
+with open('beam1_output.txt', 'r') as file:
+    for line in file:
+        # obtain harmbench advbench prompts
+        if "harmbench advbench prompts" in line.strip():
+            h_advbench = True
+        if "gemma advbench prompts" in line.strip():
+            g_advbench = True
+            h_advbench = False
+        if h_advbench == True and not "harmbench advbench prompts" in line.strip():
+            harmbench_advbench.append(line.strip())   
+        # obtain gemma advbench prompts  
+        if "harmbench jbb prompts" in line.strip():
+            g_advbench = False
+            h_jbb = True
+        if g_advbench == True and not "gemma advbench prompts" in line.strip():
+            gemma_advbench.append(line.strip())
+        # obtain harmbench jbb prompts
+        if "gemma jbb prompts" in line.strip(): 
+            h_jbb = False
+            g_jbb = True
+        if h_jbb == True and not "harmbench jbb prompts" in line.strip():
+            harmbench_jbb.append(line.strip())
+        # obtain gemma jbb prompts
+        if "rm advbench prompts" in line.strip():
+            g_jbb = False
+            rm_advbench = True
+        if g_jbb == True and not "gemma jbb prompts" in line.strip():
+            gemma_jbb.append(line.strip())
+        # obtain rm advbench prompts
+        if "rm jbb prompts" in line.strip():
+            rm_advbench = False
+            rm_jbb = True
+        if rm_advbench == True and not "rm advbench prompts" in line.strip():
+            rm_advbench_list.append(line.strip())
+        # obtain rm jbb prompts
+        if rm_jbb == True and not "rm jbb prompts" in line.strip():
+            rm_jbb_list.append(line.strip())
+
+# print(harmbench_advbench)
+# print(len(harmbench_advbench))
+# print(gemma_advbench)
+# print(len(gemma_advbench))
+# print(harmbench_jbb)
+# print(len(harmbench_jbb))
+# print(len(gemma_jbb))
+# print(gemma_jbb)
+# print(len(rm_advbench_list))
+# print(rm_jbb_list)
+# print(len(rm_jbb_list))
+
+harmbench_advbench_sampling = []
+gemma_advbench_sampling = []
+harmbench_jbb_sampling = []
+gemma_jbb_sampling = []
+rm_advbench_list_sampling = []
+rm_jbb_list_sampling = []
+
+h_advbench = False
+g_advbench = False
+h_jbb = False
+g_jbb = False
+rm_advbench = False
+rm_jbb = False
+
+# prompt file for sampling
+with open('sampling.txt', 'r') as file:
+    for line in file:
+        # obtain harmbench advbench prompts
+        if "harmbench advbench prompts" in line.strip():
+            h_advbench = True
+        if "gemma advbench prompts" in line.strip():
+            g_advbench = True
+            h_advbench = False
+        if h_advbench == True and not "harmbench advbench prompts" in line.strip():
+            harmbench_advbench_sampling.append(line.strip())   
+        # obtain gemma advbench prompts  
+        if "harmbench jbb prompts" in line.strip():
+            g_advbench = False
+            h_jbb = True
+        if g_advbench == True and not "gemma advbench prompts" in line.strip():
+            gemma_advbench_sampling.append(line.strip())
+        # obtain harmbench jbb prompts
+        if "gemma jbb prompts" in line.strip(): 
+            h_jbb = False
+            g_jbb = True
+        if h_jbb == True and not "harmbench jbb prompts" in line.strip():
+            harmbench_jbb_sampling.append(line.strip())
+        # obtain gemma jbb prompts
+        if "rm advbench prompts" in line.strip():
+            g_jbb = False
+            rm_advbench = True
+        if g_jbb == True and not "gemma jbb prompts" in line.strip():
+            gemma_jbb_sampling.append(line.strip())
+        # obtain rm advbench prompts
+        if "rm jbb prompts" in line.strip():
+            rm_advbench = False
+            rm_jbb = True
+        if rm_advbench == True and not "rm advbench prompts" in line.strip():
+            rm_advbench_list_sampling.append(line.strip())
+        # obtain rm jbb prompts
+        if rm_jbb == True and not "rm jbb prompts" in line.strip():
+            rm_jbb_list_sampling.append(line.strip())
+
+
+# overlap rm advbench prompts between beam=1 and sampling case
+overlap_rm_adv_prompts = set(rm_advbench_list) & set(rm_advbench_list_sampling)
+# remove new lines or empty lines
+rm_advbench_list = [x for x in rm_advbench_list if x.strip()]
+rm_advbench_list_sampling = [x for x in rm_advbench_list_sampling if x.strip()]
+overlap_rm_adv_prompts = [x for x in overlap_rm_adv_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_1 = len(rm_advbench_list) + len(rm_advbench_list_sampling) - len(overlap_rm_adv_prompts)
+
+# overlap rm jbb prompts
+overlap_rm_jbb_prompts = set(rm_jbb_list) & set(rm_jbb_list_sampling)
+# remove new lines or empty lines
+rm_jbb_list = [x for x in rm_jbb_list if x.strip()]
+rm_jbb_list_sampling = [x for x in rm_jbb_list_sampling if x.strip()]
+overlap_rm_jbb_prompts = [x for x in overlap_rm_jbb_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_2 = len(rm_jbb_list) + len(rm_jbb_list_sampling) - len(overlap_rm_jbb_prompts)
+
+# overlap harmbench advbench prompts
+overlap_harmbench_adv_prompts = set(harmbench_advbench) & set(harmbench_advbench_sampling)
+# remove new lines or empty lines
+harmbench_advbench = [x for x in harmbench_advbench if x.strip()]
+harmbench_advbench_sampling = [x for x in harmbench_advbench_sampling if x.strip()]
+overlap_harmbench_adv_prompts = [x for x in overlap_harmbench_adv_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_3 = len(harmbench_advbench) + len(harmbench_advbench_sampling) - len(overlap_harmbench_adv_prompts)
+
+# overlap gemma advbench prompts
+overlap_gemma_adv_prompts = set(gemma_advbench) & set(gemma_advbench_sampling)
+# remove new lines or empty lines
+gemma_advbench = [x for x in gemma_advbench if x.strip()]
+gemma_advbench_sampling = [x for x in gemma_advbench_sampling if x.strip()]
+overlap_gemma_adv_prompts = [x for x in overlap_gemma_adv_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_4 = len(gemma_advbench) + len(gemma_advbench_sampling) - len(overlap_gemma_adv_prompts)
+
+# overlap harmbench jbb prompts
+overlap_harmbench_jbb_prompts = set(harmbench_jbb) & set(harmbench_jbb_sampling)
+# remove new lines or empty lines
+harmbench_jbb = [x for x in harmbench_jbb if x.strip()]
+harmbench_jbb_sampling = [x for x in harmbench_jbb_sampling if x.strip()]
+overlap_harmbench_jbb_prompts = [x for x in overlap_harmbench_jbb_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_5 = len(harmbench_jbb) + len(harmbench_jbb_sampling) - len(overlap_harmbench_jbb_prompts)
+
+# overlap gemma jbb prompts
+overlap_gemma_jbb_prompts = set(gemma_jbb) & set(gemma_jbb_sampling)
+# remove new lines or empty lines
+gemma_jbb = [x for x in gemma_jbb if x.strip()]
+gemma_jbb_sampling = [x for x in gemma_jbb_sampling if x.strip()]
+overlap_gemma_jbb_prompts = [x for x in overlap_gemma_jbb_prompts if x.strip()]
+# calculate the unique items for the jaccard similarity
+unique_items_6 = len(gemma_jbb) + len(gemma_jbb_sampling) - len(overlap_gemma_jbb_prompts)
+
+# print jaccard similarity
+print(f"overlap rm advbench prompts = {len(overlap_rm_adv_prompts)}/{unique_items_1} ({(len(overlap_rm_adv_prompts)/unique_items_1 ) * 100}%)")
+print(f"overlap rm jbb prompts = {len(overlap_rm_jbb_prompts)}/{unique_items_2} ({(len(overlap_rm_jbb_prompts)/unique_items_2 ) * 100}%)")
+print(f"overlap harmbench advbench prompts = {len(overlap_harmbench_adv_prompts)}/{unique_items_3} ({(len(overlap_harmbench_adv_prompts)/unique_items_3 ) * 100}%)")
+print(f"overlap gemma advbench prompts = {len(overlap_gemma_adv_prompts)}/{unique_items_4} ({(len(overlap_gemma_adv_prompts)/unique_items_4 ) * 100}%)")
+print(f"overlap harmbench jbb prompts = {len(overlap_harmbench_jbb_prompts)}/{unique_items_5} ({(len(overlap_harmbench_jbb_prompts)/unique_items_5 ) * 100}%)")
+print(f"overlap gemma jbb prompts = {len(overlap_gemma_jbb_prompts)}/{unique_items_6} ({(len(overlap_gemma_jbb_prompts)/unique_items_6 ) * 100}%)")
